72738dc6cbf83c78913134abd914de18ff593321
reddaGz <44gebre@gmail.com>
2022-01-12 21:35:21 -0600

    updated

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express=require('express')
 const app=express();
-const MongoClient = require('mongodb').MongoClient
+// const MongoClient = require('mongodb').MongoClient
 // const fs = require('fs');
 // const ca = [fs.readFileSync("rds-combined-ca-bundle.pem")];
 
 
-MongoClient.connect(
-        url, {
-            sslValidate: true,
-            sslCA: "",
-            useNewUrlParser: true
-        },
-        function (err,client) {
-            console.log(err+" , "+ client);
-        });
+// MongoClient.connect(
+//         url, {
+//             sslValidate: true,
+//             sslCA: "",
+//             useNewUrlParser: true
+//         },
+//         function (err,client) {
+//             console.log(err+" , "+ client);
+//         });
 
-app.get('',(req,res,next)=>{
+app.get('/',(req,res,next)=>{
     res.send("This my first app")
 })
 app.get('/users',(req,res,next)=>{
